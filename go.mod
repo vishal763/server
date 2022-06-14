ed436bd5d052aed71086e4e49de3de245f3359a9
vishal763 <vishal.77763@gmail.com>
2022-06-14 12:09:55 +0300

    adding go file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vishal763/server
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import(
+
+	"githu.com/vishal763/server/pkg/apis/vishal.dev/v1alpha1"
+)
+
+func main () {
+	s:=v1aplha1.server()
+	fmt.Println(s)
+
+}
